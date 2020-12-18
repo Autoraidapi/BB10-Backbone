2ad14eb9f78363a339bf10efdd13f016d29918c0
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-17 17:39:20 -0800

    Update

--- src/application/assets/js/views/item.js
+++ src/application/assets/js/views/item.js
@@ -1,28 +1,19 @@
 define(function() {
 
     Backbone.View.extend({
+        
         preinitialize : function(){
             // list noop functions
         },
 
-        // All Events that sinkdown from this layer of the apps/DOM topology
         events : {},
 
         initialize : function(){
-            // Emitter responses from changes in our persistence layer
             this.listenTo();
             this.listenTo();
             this.listenTo();
-        },
+        }
 
-        // one
-
-        // all
-
-        // generator
-
-        // creation(genereator)
-
-    });
+    })
     
 });
