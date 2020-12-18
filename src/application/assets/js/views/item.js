d44203bed29eb11b5f1273fb7a9dafc7c989851d
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-17 18:09:35 -0800

    Update item.js

--- src/application/assets/js/views/item.js
+++ src/application/assets/js/views/item.js
@@ -10,6 +10,20 @@ define(['link!templates/item.html'],function(template) {
         
         events : {},
 
+        initialize : function(){
+            this.listenTo({}, 'change', this.render);
+            this.listenTo({}, 'destroy', this.remove);
+            /* */
+            this.listenTo({},  'visible', this.toggleVisible);
+        },
 
+        render : function(){
+            
+            // this.template(this.model.toJSON());
+            
+            return this;
+        }
+    
+    })
     
 });
