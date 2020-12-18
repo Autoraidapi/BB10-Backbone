67c01f6024183530d4cb8f81f91d9fcf71cff237
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-17 22:45:59 -0800

    Create plugin.js

---
+++ src/application/assets/js/plugin.js
@@ -0,0 +1,35 @@
+const Section = Backbone.View.extend({
+    template: _.template('\
+        <% if(obj.ext.match(/svg|png|ico|jpg|jpeg/)){ %>\
+            "<%= obj.src %>.<%= obj.ext %>"\
+        <% } %>\
+    '),
+    render: function (src, ext) {
+        this.$el.html(this.template({
+            src: src,
+            ext: ext
+        }));
+        return this;
+    }
+});
+const Container = Backbone.View.extend({
+    el: $('#main'),
+    initialize: function () {
+        this.$article = this.$('article');
+    },
+    renderSection : function (src, ext) {
+        var view = new Section();
+        this.$article.html(view.render(src, ext).el);
+    }
+});
+const Router = Backbone.Router.extend({
+    initialize : function(){
+        this.container = new Container();
+    },
+    routes: {
+        'image(/:src)(/:ext)': 'image'
+    },
+    image : function (src, ext) {
+        this.container.renderSection(src, ext)
+    }
+});
