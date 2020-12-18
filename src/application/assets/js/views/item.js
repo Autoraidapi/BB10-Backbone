8a7f25b9e91f0256374e8d89b3eb8e726f9d3de7
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-17 17:53:13 -0800

    Update item.js

--- src/application/assets/js/views/item.js
+++ src/application/assets/js/views/item.js
@@ -1,17 +1,26 @@
-define(function() {
+define(['link!templates/item.html'],function(template) {
 
     Backbone.View.extend({
         
         preinitialize : function(){
             // list noop functions
         },
-
+        
+        template : _.template($(template).html()),
+        
         events : {},
 
         initialize : function(){
+            
             this.listenTo();
             this.listenTo();
             this.listenTo();
+        },
+        render : function(){
+            
+            // this.template(this.model.toJSON());
+            
+            return this;
         }
 
     })
