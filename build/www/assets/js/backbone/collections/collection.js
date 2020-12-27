a548ea44a7b6f044f2b423a4d90c5135ea35298e
Russell <35252806+RussellAbraham@users.noreply.github.com>
2020-12-26 18:33:52 -0800

    Update

--- build/www/assets/js/backbone/collections/collection.js
+++ build/www/assets/js/backbone/collections/collection.js
@@ -1,14 +1,31 @@
 define(['localforageBackbone','assets/js/backbone/models/model'],function(Sync, Model) {
 
         var Collection = Backbone.Collection.extend({
-            preinitialize : function(){
-    
-            },
+            preinitialize : function(){},
+                
             model : Model,
             sync : Backbone.localforage.sync('collection'),
             initialize : function(){
     
-            }
+            },
+            print : function(){
+		var i, length = this.length;
+		for(i = length - 1;i >= 0;i--){
+			console.log(
+				JSON.stringify(this.models[i].toJSON(), null, 2)
+			);
+		}            
+            },
+            sprint : function(){ this.printer(this.length); },
+            printer : function(){
+		var self = this;
+		if(length <= 0) return;
+		length--;
+		setTimeout(function(){
+			self.printer(length);
+			this.models[length].toJSON();
+		}, 25);            
+            },                
         });
     
         return Collection;
